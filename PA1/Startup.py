f8c7b7e2a0cc8fa96447638806eb516139a1c8af
ShabbyOWEN <owenchouerzi@gmail.com>
2019-10-05 14:45:26 -0700

    [PA1] Add Python starter code

---
+++ PA1/Startup.py
@@ -0,0 +1,90 @@
+"""
+Starter code for PA1 programming problems.
+Author:
+Date:
+"""
+
+class pa1_starter():
+    """
+    pa1_starter class for containing starter methods and descriptions.
+    """
+
+    def missing_twin(self, lst):
+        """
+        Given a non-empty list of numbers, find the digit that is not part of a pair.
+
+        Input: non-empty list of numbers.
+        Return: number that does not have a pair.
+        """
+        pass
+
+    def products_list(self, lst):
+        """
+        Given a list of numbers, return a list where each element is the product
+        of all other elements in the list, excluding the one at its index.
+
+        Input: list of numbers.
+        Return: list of multiplications of full list, excluding current digit.
+        """
+        pass
+
+    def last_word(self, string):
+        """
+        Given a string of characters, find the last word in the string.
+
+        Input: a string.
+        Return: last word in string that is made of only alphabetic characters.
+        """
+        pass
+
+
+    def pretty_patterns(self, n):
+        """
+        Given an integer less than 99, print a symmetric triangle with n rows,
+        where each row k is made of the numbers from 1 to k.
+
+        Input: an integer.
+        Return: symmetric triangle with n rows.
+        """
+        pass
+
+    def move_negatives(self, lst):
+        """
+        Given a list of numbers, return a list with all positive numbers in the
+        front of their array in their original order, and replace all negative
+        elements with 0, which come after the positive values.
+
+        Input: a list of numbers.
+        Returns: list of numbers with positives in original order, then negatives
+                replaced with 0.
+        """
+        pass
+
+    def feed_cows(self, cows, k):
+        """
+        Given a list of numbers representing cows that need n cups of food
+        at each space, find out how many steps you need to walk to feed all the cows
+        if you have a bucket that can hold maximum k cups of food at a time. To refill 
+        your bucket, you must walk to index -1, that is, you must take one step passed
+        the first cow to reach the food supply.
+
+        You should always walk as far as you can with your bucket. If you empty your
+        bucket at one cow and that cow is fed, you should still move forward to check 
+        if you need to feed the next cow.
+
+        Input: a list representing the cups of food a cow needs and a number 
+        representing bucket capacity.
+        Return: how many steps it takes to feed all the cows.
+        """
+        pass
+
+    def k_closest_point(self, points, k, center):
+        """
+        Given an array of points, a number of how man points to return, k, and
+        the center of your search area, return a list of k points that are closest
+        to your center. Distances are to be calculated using eucledian distance.
+
+        Input: a list of coordinates, a number of points to find, a center coordinate.
+        Return: a list of the k closest coordinates in points to center.
+        """
+        pass
